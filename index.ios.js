83569b6429853dd64733ee644d7108e06be15b5c
rsstdd <rosstod@gmail.com>
2017-02-05 15:45:56 -0800

    Styled Header

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
 
-const App = () => {
-  return (
-    <Text>Some text</Text>
-  );
-};
+const App = () => (
+  <Header headerText={'Albums!'} />
+);
 
 AppRegistry.registerComponent('albums', () => App);
