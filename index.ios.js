2485d679032b203432dc683f9539191225c42b05
rsstdd <rosstod@gmail.com>
2017-02-05 11:52:33 -0800

    Init Commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Some text</Text>
+  );
+};
+
+AppRegistry.registerComponent('albums', () => App);
